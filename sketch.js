58824641fc01d1832f29ade0525a99c087812b96
Rivan Faitulloh <faitullohjr64@gmail.com>
2024-07-06 11:42:29 +0700

    project p5

---
+++ sketch.js
@@ -0,0 +1,21 @@
+//Rivan Faitulloh
+function setup() {
+  createCanvas(1500, 500, WEBGL);
+}
+function preload(){
+  bumi = loadImage('bumi.jpg');
+  bulan = loadImage('bulan.jpg');
+
+}
+function draw() {
+  background(50);
+  texture(bumi);
+  orbitControl();
+  rotateY(millis()/2500);
+  sphere(100);
+  rotateY(millis()/300);
+  translate(10,10,150);
+  texture(bulan);
+  sphere(30);
+  noStroke();
+}
